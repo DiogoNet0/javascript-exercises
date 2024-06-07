57a8a37c9ae4fbd24da8c925bba34b9d3fc26d29
DiogoNet0 <diogoneto1779@gmail.com>
2024-06-07 17:07:39 +0100

    Solved the first exercise of the exercises and started the second one

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  function string(a) {
+    return "hey";
+  }
+  function num() {}
 };
 
 // Do not edit below this line
